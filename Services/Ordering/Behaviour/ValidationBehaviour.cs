396b4abb6618863b64d2b589f5f3f413fd739b31
rahulsahay19 <rahulsahay19@hotmail.com>
2025-08-04 20:39:16 +0530

    Till Order Context Factory

---
+++ Services/Ordering/Behaviour/ValidationBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Ordering.Behaviour
+{
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+                //Run all the validations one by one and return the Validation Result
+                var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                //now need to check for any failures
+                var failures = validationResults.SelectMany(e => e.Errors).Where(f => f != null).ToList();
+                if (failures.Count > 0)
+                {
+                    throw new ValidationException(failures);
+                }
+            }
+            //On success case
+            return await next();
+        }
+    }
+}
---
+++ Services/Ordering/Data/OrderContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Ordering.Data
+{
+    public class OrderContextFactory : IDesignTimeDbContextFactory<OrderContext>
+    {
+        public OrderContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("OrderingConnectionString");
+            var optionsBuilder = new DbContextOptionsBuilder<OrderContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+            return new OrderContext(optionsBuilder.Options);
+        }
+    }
+}
