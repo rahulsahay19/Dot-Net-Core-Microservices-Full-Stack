5eeb37c69725a9ef2f0470d76d764763b6f7c74d
rahulsahay19 <rahulsahay19@hotmail.com>
2025-09-10 18:52:17 +0530

    added breadcrumb

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
-import { Store } from './store/store/store';
 import { ServerError } from './core/server-error/server-error';
 import { UnAuthenticated } from './core/un-authenticated/un-authenticated';
 import { NotFound } from './core/not-found/not-found';
